037cc84958ac748dd4cf7e705e0c23c991575c06
illavarasi <illavarasi@mercu.io>
2025-10-31 13:41:22 +0530

    updated align

--- frontend/src/pages/ProductDetails.tsx
+++ frontend/src/pages/ProductDetails.tsx
@@ -106,7 +106,7 @@ const ProductDetails = () => {
                     <p className="text-3xl font-medium">Related Products</p>
                     <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
                 </div>
-                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6 w-full">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3 md:gap-6 mt-6 w-full">
                     {relatedProducts.filter((product) => product.inStock).map((product, index) => (
                         <ProductCard key={index} product={product} />
                     ))}</div>
